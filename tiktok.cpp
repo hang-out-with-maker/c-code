3164e58d600eb60932b50ba6e46f64a68abff862
gokimkq123 <62462552+gokimkq123@users.noreply.github.com>
2020-11-11 15:11:49 +0900

    Update tiktok.cpp

--- tiktok.cpp
+++ tiktok.cpp
@@ -1,4 +1,3 @@
-
 int sc(int index){		//평가함수 플레이어 에게 유리할때는 양수, AI에게 유리할때는 음수
     int temp=0,s=0,row=3;
 
@@ -6,28 +5,32 @@ int sc(int index){		//평가함수 플레이어 에게 유리할때는 양수, A
         for(int i=1;i<=3;i++){
             if(board[index][i][j]==1) temp++;
         }
-        if(temp==2) s -= 1000000;
+        if(temp==3)  s -= 1000000;
+        else if(temp==2) s -= 100;
         else if(temp==1) s -= 1;
     }
     for(int j=1;j<=3;j++){
         for(int i=1;i<=3;i++){
             if(board[index][j][i]==1) temp++;
         }
-        if(temp==2) s -= 1000000;
+        if(temp==3)  s -= 1000000;
+        else if(temp==2) s -= 100;
         else if(temp==1) s -= 1;
     }
 
     for(int i=1;i<=3;i++){
         if(board[index][i][i]==1) temp++;
     }
-    if(temp==2) s -= 1000000;
+    if(temp==3)  s -= 1000000;
+    else if(temp==2) s -= 100;
     else if(temp==1) s -= 1;
 
     row=3;
     for(int i=1;i<=3;i++){
         if(board[index][i][row--]==1) temp++;
     }
-    if(temp==2) s -= 1000000;
+    if(temp==3)  s -= 1000000;
+    else if(temp==2) s -= 100;
     else if(temp==1) s -= 1;
 
 
@@ -36,28 +39,35 @@ int sc(int index){		//평가함수 플레이어 에게 유리할때는 양수, A
         for(int i=1;i<=3;i++){
             if(board[index][i][j]==2) temp++;
         }
-        if(temp==2) s += 1000000;
+        if(temp==3)  s += 1000000;
+        else if(temp==2) s += 100;
         else if(temp==1) s += 1;
     }
     for(int j=1;j<=3;j++){
         for(int i=1;i<=3;i++){
             if(board[index][j][i]==2) temp++;
         }
-        if(temp==2) s += 1000000;
+
+        if(temp==3)  s += 1000000;
+        else if(temp==2) s += 100;
         else if(temp==1) s += 1;
     }
 
     for(int i=1;i<=3;i++){
         if(board[index][i][i]==1) temp++;
     }
-    if(temp==2) s += 1000000;
+
+    if(temp==3)  s += 1000000;
+    else if(temp==2) s += 100;
     else if(temp==1) s += 1;
 
     row=3;
     for(int i=1;i<=3;i++){
         if(board[index][i][row--]==1) temp++;
     }
-    if(temp==2) s += 1000000;
+
+    if(temp==3)  s += 1000000;
+    else if(temp==2) s += 100;
     else if(temp==1) s += 1;
 
     return s;
